426d600fb546ea89d0f9bb7d3ff9e7bdc89f342d
AyushKeshari213 <153152247+AyushKeshari213@users.noreply.github.com>
2024-12-13 20:39:26 +0530

    Update Question 5.py

--- Question 5.py
+++ Question 5.py
@@ -1,1 +1,8 @@
+# Impute missing gender values with mode
+mode_gender = df_patient['gender'].mode()[0]
+df_patient['gender'].fillna(mode_gender, inplace=True)
+
+# Calculate percentage of females
+female_percentage = ((df_patient['gender'] == 'F').sum() / len(df_patient)) * 100
+print(round(female_percentage, 2))  # Output: 32.26
 
